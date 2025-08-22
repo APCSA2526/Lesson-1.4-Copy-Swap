1ae9463a8113fe566f534f8106a594bae7c7d029
Alyssa Gee <a.gee.educator@gmail.com>
2025-08-22 20:58:51 +0000

    save files

---
+++ Swap.java
@@ -0,0 +1,31 @@
+/*
+AP CS A
+Name: Ms. Gee
+Date: Aug 22, 2025
+Teacher: Ms. Gee
+Assignment: Lesson 1.4 Notes
+Description:
+    * Based on the code to "copy" variables, write code
+    * that swaps the value two int variables x and y.
+    * Use java.util.Scanner to prompt values for int
+    * variables x and y.
+    * Hint: you might need to create new variable(s).
+*/
+
+
+public class Swap {
+    public static void main(String[] args){
+
+        int x = 0;
+        int y = 1;
+        System.out.println("Before swap x: " + x);
+        System.out.println("Before swap y: " + y);
+        
+        System.out.println("Swapping...");
+        
+        /* to be implemented */
+        
+        System.out.println("After swap x: " + x);
+        System.out.println("After swap y: " + y);
+    }
+}
